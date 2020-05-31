3703381c8939f77d0f66571438883c9270847ea2
DropCode <primtech.co@gmail.com>
2020-05-31 03:41:20 +0100

    update api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default () => {
     return axios.create({
         // baseURL: `http://127.0.0.1:8000/api/v1/`
-        // baseURL: `http://67.205.138.181/api/v1/`,
+           baseURL: `http://67.205.138.181/api/v1/`,
     })
 }
