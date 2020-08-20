886e34eaeed71a56db34cde2f3f52dcbc15e1b85
Lord-AY <ayomidealadesulu@gmail.com>
2020-08-20 23:22:18 +0100

    fix: registration validation

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () => {
     return axios.create({
-        // baseURL: process.env.VUE_APP_APIURL
+        baseURL: process.env.VUE_APP_APIURL
         // baseURL: "http://127.0.0.1:8000/api/v1"
     });
 };
